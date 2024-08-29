6c3919774e1c25a60882f3a2aa9139b33b9cef85
eudald-guillen-klarrio <eudald.guillen@klarrio.com>
2024-08-29 16:06:25 +0200

    taskwriter

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"strconv"
 	"os"
 	"strings"
@@ -11,27 +10,37 @@ import (
 
 type task struct{
 	name string
-	deadline time.Time
 	status bool //true == done false == not done
-	ood bool //is not done and past the deadline
 }
 
 var tasksToDo []task
 var tasksDone []task
 
-func newTask(name string, deadline time.Time, status bool) task {
+func newTask(name string, status bool) task {
 	var t task
 	t.name = name
-	t.deadline = deadline
 	t.status = status
-	t.ood = false
 	return t
+}
+
+func createTask (taskname string){
+	t := newTask(taskname, false)
+	taskWriter(t)
+	taskLoader(todoParser())
+}
+
+
+func taskWriter(task task){
+	i, err := os.Open("./todos")
+	defer i.Close()
+	if err != nil {fmt.Println(err)}
+	
+	i.Write([]byte(fmt.Sprintf("%v", tasklist)) )
 }
 
 func taskLoader(tasklist []task){
 	for _, task := range tasklist{
 		if task.status {tasksDone = append(tasksDone, task)} 
-		if !task.status && task.deadline.Compare(time.Now()) < 1 {task.ood = true}
 	 	if !task.status {tasksToDo = append(tasksToDo, task)}
 	}
 }
@@ -52,9 +61,7 @@ func todoParser() []task {
 		}
 		
 		t.name = fields[0]
-		t.deadline, _ = time.Parse("2006-01-02", "2023-02-03")
 		t.status, _ = strconv.ParseBool(fields[2])
-		t.ood, _ = strconv.ParseBool(fields[3])
 
 		tasks = append(tasks, t)
 	}
@@ -62,18 +69,11 @@ func todoParser() []task {
 	
 }
 
-func createTask (taskname string, deadline string){
-	dl, err := time.Parse("2006-01-02", deadline)
-	if err != nil {fmt.Println(err)}
 
-	newTask(taskname, dl, false)
-	//writetofile
-	taskLoader(todoParser())
-}
 
 func main(){
 	taskLoader(todoParser())
-	
+	createTask("task1")
 	// fmt.Println(tasks)
 	fmt.Println("these are the done tasks: ", tasksDone)
 	fmt.Println("thse are the tasks to do: ", tasksToDo)
